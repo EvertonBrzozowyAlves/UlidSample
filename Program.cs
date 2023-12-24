5986012c13b22b6f9c82dbd0499a527fb6b11820
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2023-12-24 11:29:58 -0300

    first

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿var ulidList = new List<Ulid>();
+
+for (var i = 0; i < 10; i++)
+{
+    var generatedUlid = Ulid.NewUlid();
+    Console.WriteLine("Generated one: " + generatedUlid);
+    ulidList.Add(generatedUlid);
+}
+
+Console.WriteLine("");
+Console.WriteLine("Before sort:");
+Console.WriteLine("");
+
+
+ulidList.ForEach(x => Console.WriteLine(x));
+
+ulidList.Reverse();
+
+Console.WriteLine("");
+Console.WriteLine("After Revert:");
+Console.WriteLine("");
+
+ulidList.ForEach(x => Console.WriteLine(x));
